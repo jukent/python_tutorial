b9d88869a0a5a0543a7ed7e92ecb5a37618476e3
Julia Kent <jkent@ucar.edu>
2021-02-10 13:12:57 -0700

    Using list comprehensions

--- heatindexcomp.py
+++ heatindexcomp.py
@@ -12,10 +12,7 @@ types = {'tempout' : float, 'humout' : float, 'heatindex' : float}
 data = read_data(columns, types=types)
 
 # Compute the heat index
-heatindex = []
-for temp, humout in zip(data['tempout'], data['humout']):
-    heatindex.append(compute_heatindex(temp, humout))
-
+heatindex = [compute_heatindex(t,h) for t, h in zip(data['tempout'], data['humout'])]
 
 # Output comparison of data
 print_comparison('HEAT INDX', data['date'], data['time'], data['heatindex'], heatindex)
